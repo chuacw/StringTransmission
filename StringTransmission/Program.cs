c7933ae04305c872cf4016dc38fdae3152a2bdc1
CHUA Chee Wee <github@chuacw.ath.cx>
2013-12-17 17:12:01 +0800

    Initial check-in

---
+++ StringTransmission/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace StringTransmission
+{
+    class Solution
+    {
+        static StreamReader sr;
+
+        static int ReadNumber()
+        {
+            StringBuilder sb = new StringBuilder();
+            int lNumber = 0; char ch = ' ';
+
+            do
+            {
+                int i = sr.Read();
+                if (i == -1) break;
+                ch = Convert.ToChar(i);
+            } while (!Char.IsNumber(ch));
+            sb.Append(ch);
+
+            do
+            {
+                int i = sr.Read();
+                if (i == -1) break;
+                ch = Convert.ToChar(i);
+                if (Char.IsNumber(ch)) sb.Append(ch);
+            } while (Char.IsNumber(ch));
+
+            lNumber = Convert.ToInt32(sb.ToString());
+            return lNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            sr = new StreamReader(Console.OpenStandardInput());
+            int T = ReadNumber();
+            for (int i = 1; i <= T; i++)
+            {
+                int N = ReadNumber();
+                int K = ReadNumber();
+
+            }
+        }
+    }
+}
